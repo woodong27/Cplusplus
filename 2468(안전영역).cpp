f833ed3a7396b68af144d5be79baf8a36606b911
woodong27 <cdw5548@gmail.com>
2023-04-06 23:26:52 +0900

    04/06/2023

---
+++ 2468(안전영역).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+int N;
+int map[101][101];
+int visited[101][101];
+
+int main() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+		}
+	}
+
+	return 0;
+}
