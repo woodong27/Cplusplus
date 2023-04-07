360a2ad3df7f37820a9da8a3dfb6f71eececb092
woodong27 <cdw5548@gmail.com>
2023-04-07 13:53:14 +0900

    04/07/2023

--- 13913(숨바꼭질4).cpp
+++ 13913(숨바꼭질4).cpp
@@ -1,72 +1,60 @@
 #include <iostream>
-#include <algorithm>
-#include <queue>
 #include <deque>
-
 using namespace std;
 
-int N, K;
-int visited[100001] = { -1 };
-
-int moving(int selection, int cur) {
-	if (selection == 0) {
-		return cur * 2;
-	}
-	else if (selection == 1) {
-		return cur + 1;
-	}
-	else {
-		return cur - 1;
-	}
-}
-
 int main() {
+    int N, K;
+    cin >> N >> K;
 
-	cin >> N >> K;
+    int visited[100001];
+    for (int i = 0; i < 100001; i++) {
+        visited[i] = -1;
+    }
+    visited[N] = N;
+    int ans = 0;
 
-	int ans = 0;
+    deque<pair<int, int>> q;
+    q.push_back(make_pair(N, 0));
 
-	//queue선언해서 배열 넣어주기
-	queue<pair<int, int>> q;
-	q.push(pair<int, int>(N, 0));
+    while (!q.empty()) {
+        int current = q.front().first;
+        int cnt = q.front().second;
+        q.pop_front();
 
-	//cout << q.front().first;
-	//cout << q.front().second;
-	while (!q.empty()) {
-		int current = q.front().first;
-		int cnt = q.front().second;
-		q.pop();
-		if (current == K) {
-			ans = cnt;
-			break;
-		}
-		for (int i = 0; i < 3; i++) {
-			int nextp = moving(i, current);
-			if ((nextp >= 0 && nextp <= 100000) && visited[nextp] == -1) {
-				visited[nextp] = current;
-				q.push(pair<int, int>(nextp, cnt + 1));
-			}
-		}
-	}
+        if (current == K) {
+            ans = cnt;
+            break;
+        }
 
-	cout << ans << endl;
+        int next[3] = { current * 2, current + 1, current - 1 };
+        for (int i = 0; i < 3; i++) {
+            if (0 <= next[i] && next[i] <= 100000 && visited[next[i]] == -1) {
+                visited[next[i]] = current;
+                q.push_back(make_pair(next[i], cnt + 1));
+            }
+        }
+    }
 
-	deque<int> path;
-	int start = K;
-	while (1) {
-		path.push_front(start);
-		start = visited[start];
-		if (start == N) {
-			break;
-		}
-	}
-	if (path.front() != N) {
-		path.push_front(N);
-	}
-	
-	for (int i = 0; i < path.size(); i++) {
-		cout << path[i];
-	}
-	
-	return 0;
+    cout << ans << endl;
+
+    deque<int> path;
+    int start = K;
+
+    while (true) {
+        path.push_front(start);
+        start = visited[start];
+        if (start == N) {
+            break;
+        }
+    }
+
+    if (path.front() != N) {
+        path.push_front(N);
+    }
+
+    for (int i = 0; i < path.size(); i++) {
+        cout << path[i] << " ";
+    }
+
+    return 0;
 }
