9fe6b5f1b386147b0167e8f968594fbad7c237db
woodong27 <cdw5548@gmail.com>
2023-04-14 17:45:17 +0900

    04/14/2023

--- 13460(구슬탈출2).cpp
+++ 13460(구슬탈출2).cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 int N, M;
 char board[11][11];
-int visited[10][10] = { 0 };
+int visited_red[10][10] = { 0 };
+int visited_blue[10][10] = { 0 };
 int si_red, sj_red, si_blue, sj_blue, ei,ej;
 int di[] = { 0,0,-1,1 };
 int dj[] = { -1,1,0,0 };
@@ -18,32 +19,63 @@ int moving(int rsi, int rsj, int bsi, int bsj) {
 	redq.push_back({ rsi,rsj });
 	blueq.push_back({ bsi,bsj });
 
-	int cnt_red;
-	visited[rsi][rsj] = 1;
+	int cnt_red = 0;
+	int cnt_blue = 0;
+	visited_red[rsi][rsj] = 1;
+	visited_blue[bsi][bsj] = 1;
 	while (!redq.empty()) {
-		int ci = redq.front().first;
-		int cj = redq.front().second;
-		cout << ci << cj;
-		if (board[ci][cj] == 'O') {
-			cnt_red = visited[ci][cj];
-			break;
+		int rci = redq.front().first;
+		int rcj = redq.front().second;
+		if (rci == ei && rcj == ej) {
+			cnt_red = visited_red[rci][rcj];
 		}
 		for (int i = 0; i < 4; i++) {
 			for (int m = 1; m < 10; m++) {
-				int ni = ci + m * di[i];
-				int nj = cj + m * dj[i];
-				if (board[ni][nj] != '#' && board[ni][nj] != 'B' && !visited[ni][nj]) {
-					visited[ni][nj] = visited[ci][cj]+1;
+				int rni = rci + di[i] * m;
+				int rnj = rcj + dj[i] * m;
+				if (board[rni][rnj] == '.' && !visited_red[rni][rnj]) {
+					visited_red[rni][rnj] = visited_red[rci][rcj] + 1;
 				}
-				else if (m!=1 && board[ni][nj]=='#' && board[ni-di[i]][nj-dj[i]]=='.') {
-					redq.push_back({ ni - di[i],nj - dj[i] });
+				else if (board[rni][rnj] == 'O' && !visited_red[rni][rnj]) {
+					redq.push_back({ rni,rnj });
+					visited_red[rni][rnj] = visited_red[rci][rcj] + 1;
 				}
-
+				else if ((board[rni][rni] == '#' && m > 1) || (board[rni][rnj] == 'B' && m>1)){
+					redq.push_back({ rni - di[i],rnj - dj[i] });
+				}
 			}
 		}
 	}
-	return cnt_red;
+	while (!blueq.empty()) {
+		int bci = blueq.front().first;
+		int bcj = blueq.front().second;
+		if (bci == ei && bcj == ej) {
+			cnt_red = visited_red[bci][bcj];
+		}
+		for (int i = 0; i < 4; i++) {
+			for (int m = 1; m < 10; m++) {
+				int bni = bci + di[i] * m;
+				int bnj = bcj + dj[i] * m;
+				if (board[bni][bnj] == '.' && !visited_red[bni][bnj]) {
+					visited_red[bni][bnj] = visited_red[bci][bcj] + 1;
+				}
+				else if (board[bni][bnj] == 'O' && !visited_red[bni][bnj]) {
+					redq.push_back({ bni,bnj });
+					visited_red[bni][bnj] = visited_red[bci][bcj] + 1;
+				}
+				else if ((board[bni][bni] == '#' && m > 1) || (board[bni][bnj] == 'B' && m > 1)) {
+					redq.push_back({ bni - di[i],bnj - dj[i] });
+				}
+			}
+		}
+	}
 
+	if (cnt_red > 10 || cnt_red == cnt_blue) {
+		return -1;
+	}
+	else {
+		return cnt_red;
+	}
 }
 
 int main() {
@@ -64,7 +96,8 @@ int main() {
 		}
 	}
 
-	int red_result=moving(si_red, sj_red, si_blue, sj_blue);
-	cout << red_result;
+	int ans=moving(si_red, sj_red, si_blue, sj_blue);
+	cout << ans << endl;
+
 	return 0;
 }
