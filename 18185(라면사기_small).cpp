4eb12b0afba9ac31185b9b29f991c4e28c692556
woodong27 <cdw5548@gmail.com>
2023-04-17 23:12:25 +0900

    04/17/2023

--- 18185(라면사기_small).cpp
+++ 18185(라면사기_small).cpp
@@ -5,14 +5,38 @@
 using namespace std;
 
 int N;
-int factories[10000] = { 0 };
+int factories[100000] = { 0 };
 
 int buying() {
 	int result = 0;
-	vector<int> temp;
+	
+	for (int i = 0; i < N; i++) {
+		if (factories[i + 1] > factories[i + 2]) {
+			int minv1 = min(factories[i], factories[i + 1] - factories[i + 2]);
+			result += 5 * minv1;
+			factories[i] -= minv1;
+			factories[i + 1] -= minv1;
 
+			int minv2 = min(factories[i], min(factories[i + 1], factories[i + 2]));
+			result += 7 * minv2;
+			factories[i] -= minv2;
+			factories[i + 1] -= minv2;
+			factories[i + 2] -= minv2;
+		}
+		else {
+			int minv1 = min(factories[i], min(factories[i + 1], factories[i + 2]));
+			result += 7 * minv1;
+			factories[i] -= minv1;
+			factories[i + 1] -= minv1;
+			factories[i + 2] -= minv1;
 
-
+			int minv2 = min(factories[i], factories[i + 1]);
+			result += 5 * minv2;
+			factories[i] -= minv2;
+			factories[i + 1] -= minv2;
+		}
+		result += 3 * factories[i];
+	}
 	return result;
 }
 
