f0cea6f3c386214b9340d9aae9d695db0e9af493
woodong27 <cdw5548@gmail.com>
2023-04-17 17:53:36 +0900

    04/17/2023

---
+++ 18185(라면사기_small).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N;
+int factories[10000] = { 0 };
+
+int buying() {
+	int result = 0;
+	vector<int> temp;
+
+
+
+	return result;
+}
+
+int main() {
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> factories[i];
+	}
+
+	int ans = buying();
+
+	cout << ans << endl;
+
+	return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <deque>
 #include <tuple>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
 
-	char a = '$';
+	vector<int> temp;
+	temp.push_back(10);
+	temp.push_back(30);
+	temp.push_back(5);
 
-	cout << isalpha(a) << endl;
-	cout << islower(a) << endl;
+	int minv = *min_element(temp.begin(), temp.end());
 
-	return 0;
+	cout << minv;
 }
