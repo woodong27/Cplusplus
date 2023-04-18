5a92b35ebf0fe9f0ebbde562b86b7b3aff243d82
woodong27 <cdw5548@gmail.com>
2023-04-18 17:33:01 +0900

    04/18/2023

---
+++ 1665(가운데를말해요).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int N, num, ans;
+vector<int> numbers;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> num;
+		numbers.push_back(num);
+		//È¦¼ö ¹ø
+		if (i % 2) {
+
+		}
+		//Â¦¼ö ¹ø
+		else {
+
+		}
+	}
+
+
+	return 0;
+}
