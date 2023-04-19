c223d8e5979263c954e888611793ff8a87dd3a5d
woodong27 <cdw5548@gmail.com>
2023-04-19 22:01:48 +0900

    04/19/2023 - 벽부수고이동하기 해결

--- 2206(벽부수고이동하기).cpp
+++ 2206(벽부수고이동하기).cpp
@@ -7,53 +7,34 @@ using namespace std;
 
 int n, m;
 int map[1000][1000];
-int visited[1000][1000] = { 0 };
+int visited[1000][1000][2];
 int di[] = { 0,0,-1,1 };
 int dj[] = { -1,1,0,0 };
 
 int moving() {
 	deque<pair<pair<int, int>, int>> q;
 	q.push_back({ {0,0},0 });
-	visited[0][0] = 1;
+	visited[0][0][0] = 1;
 	while (!q.empty()) {
 		int ci = q.front().first.first;
 		int cj = q.front().first.second;
 		int broken = q.front().second;
 		if (ci == n - 1 && cj == m - 1) {
-			return visited[ci][cj];
+			return visited[ci][cj][broken];
 		}
-		cout << ci << cj << broken << endl;
 		q.pop_front();
 		for (int i = 0; i < 4; i++) {
 			int ni = ci + di[i];
 			int nj = cj + dj[i];
 			if (0 <= ni && ni < n && 0 <= nj && nj < m) {
-				//방문한 적 없을때
-				if (!visited[ni][nj]) {
-					//벽을 부순적이 없으면 그대로 넣음
+				if (!visited[ni][nj][broken]) {
 					if (map[ni][nj] == 0) {
 						q.push_back({ {ni,nj},broken });
-						visited[ni][nj] = visited[ci][cj] + 1;
+						visited[ni][nj][broken] = visited[ci][cj][broken] + 1;
 					}
-					//다음칸이 벽이고 벽을 부순적이 없다면 벽을 부수고 지나감
-					//broken+1로 벽 부순거 표시
 					else if (map[ni][nj] == 1 && !broken) {
 						q.push_back({ {ni,nj},broken + 1 });
-						visited[ni][nj] = visited[ci][cj] + 1;
-					}
-				}
-				//방문한 적 있을때
-				//이전에 벽을 부수고 방문했는데 이번에 벽을 안부수고 왔다면 갈 수 있음
-				else {
-					if (!broken) {
-						if (map[ni][nj] == 0) {
-							q.push_back({ {ni,nj},broken });
-							visited[ni][nj] = visited[ci][cj] + 1;
-						}
-						else if (map[ni][nj] == 1) {
-							q.push_back({ {ni,nj},broken + 1 });
-							visited[ni][nj] = visited[ci][cj] + 1;
-						}
+						visited[ni][nj][broken + 1] = visited[ci][cj][broken] + 1;
 					}
 				}
 			}
