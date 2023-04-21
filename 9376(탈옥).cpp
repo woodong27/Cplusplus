5fa943478c0eb33d7852b3adf781296efe1e22cf
woodong27 <cdw5548@gmail.com>
2023-04-21 17:52:09 +0900

    04/21/2023

--- 9376(탈옥).cpp
+++ 9376(탈옥).cpp
@@ -1,21 +1,39 @@
 #include <iostream>
-#include <algorithm>
 #include <deque>
 #include <cstring>
 
 using namespace std;
 
 int t, h, w;
-int visited[100][100];
-char jail[101][101];
+int visited[102][102][3];
+char jail[103][103];
 int di[] = { 0,0,-1,1 };
 int dj[] = { -1,1,0,0 };
-deque<pair<pair<int, int>,int>> prisoners_position;
+int ans;
 
-int bfs(int si, int sj, int scnt) {
-	memset(visited, 0, sizeof(visited));
-
-
+void bfs(int si, int sj, int scnt){
+	visited[si][sj][scnt] = 0;
+	deque<pair<int, int>> q;
+	q.push_back({ si,sj });
+	while (!q.empty()) {
+		int ci = q.front().first;
+		int cj = q.front().second;
+		q.pop_front();
+		for (int i = 0; i < 4; i++) {
+			int ni = ci + di[i];
+			int nj = cj + dj[i];
+			if (0 <= ni && ni < h + 2 && 0 <= nj && nj < w + 2 && visited[ni][nj][scnt] == -1) {
+				if (jail[ni][nj] == '.') {
+					q.push_front({ ni,nj });
+					visited[ni][nj][scnt] = visited[ci][cj][scnt];
+				}
+				else if (jail[ni][nj] == '#') {
+					q.push_back({ ni,nj });
+					visited[ni][nj][scnt] = visited[ci][cj][scnt] + 1;
+				}
+			}
+		}
+	}
 }
 
 int main() {
@@ -26,29 +44,42 @@ int main() {
 
 	while (t--) {
 		cin >> h >> w;
-		
-		prisoners_position.clear();
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				cin >> jail[i][j];
-				if (jail[i][j] == '$') {
-					prisoners_position.push_back({ { i,j },0 });
+
+		for (int i = 0; i < h + 2; i++) {
+			for (int j = 0; j < w + 2; j++) {
+				if (i == 0 || i == h + 1 || j == 0 || j == w + 1) {
+					jail[i][j] = '.';
+				}
+				else {
+					cin >> jail[i][j];
 				}
 			}
 		}
 
-		int result = 0;
-		int ans = h * w;
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				if (i == 0 || i == h - 1 || j == 0 || j == w - 1) {
+		memset(visited, -1, sizeof(visited));
+		bfs(0, 0, 0);
+		int cnt = 1;
+		for (int i = 0; i < h + 2; i++) {
+			for (int j = 0; j < w + 2; j++) {
+				if (jail[i][j] == '$') {
+					jail[i][j] = '.';
+					bfs(i, j, cnt);
+					cnt++;
+				}
+			}
+		}
+		
+		ans = h * w;
+		for (int i = 0; i < h + 2; i++) {
+			for (int j = 0; j < w + 2; j++) {
+				int temp;
+				if (jail[i][j] != '*') {
+					temp = visited[i][j][0] + visited[i][j][1] + visited[i][j][2];
 					if (jail[i][j] == '#') {
-						result = bfs(i, j, 1);
-						ans = min(ans, result);
+						temp -= 2;
 					}
-					else if (jail[i][j] == '.') {
-						result = bfs(i, j, 0);
-						ans = min(ans, result);
+					if (temp > 0) {
+						ans = min(ans, temp);
 					}
 				}
 			}
