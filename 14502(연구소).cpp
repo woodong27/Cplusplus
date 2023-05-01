837fb0d6ada67f3d998f5cb1d921de601ca86c48
woodong27 <cdw5548@gmail.com>
2023-05-01 15:44:16 +0900

    05/01/2023

--- 14502(연구소).cpp
+++ 14502(연구소).cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <deque>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
@@ -13,6 +14,9 @@ int di[] = { 0,0,-1,1 };
 int dj[] = { -1,1,0,0 };
 
 void bfs() {
+	int copied_map[8][8] = { 0 };
+	copy(&map[0][0], &map[0][0] + 64, &copied_map[0][0]);
+	memset(visited, 0, sizeof(visited));
 	deque<pair<int, int>> q(starts);
 	while (!q.empty()) {
 		int ci = q.front().first;
@@ -21,24 +25,24 @@ void bfs() {
 		for (int i = 0; i < 4; i++) {
 			int ni = ci + di[i];
 			int nj = cj + dj[i];
-			if (0 <= ni && ni < n && 0 <= nj && nj < m && map[ni][nj]==0 && !visited[ni][nj]) {
+			if (0 <= ni && ni < n && 0 <= nj && nj < m && !copied_map[ni][nj] && !visited[ni][nj]) {
 				q.push_back({ ni,nj });
 				visited[ni][nj] = 1;
+				copied_map[ni][nj] = 2;
 			}
 		}
 	}
-}
 
-int counting() {
 	int cnt = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (!map[i][j]) {
+			if (copied_map[i][j] == 0) {
 				cnt++;
 			}
 		}
 	}
-	return cnt;
+
+	ans = max(ans, cnt);
 }
 
 int main() {
@@ -51,7 +55,6 @@ int main() {
 			cin >> map[i][j];
 			if (map[i][j] == 2) {
 				starts.push_back({ i,j });
-				visited[i][j] = 1;
 			}
 		}
 	}
@@ -61,15 +64,14 @@ int main() {
 			if (map[i][j] == 0) {
 				map[i][j] = 1;
 				for (int ii = i; ii < n; ii++) {
-					for (int jj = j + 1; jj < m; jj++) {
+					for (int jj = 0; jj < m; jj++) {
 						if (map[ii][jj] == 0) {
 							map[ii][jj] = 1;
 							for (int iii = ii; iii < n; iii++) {
-								for (int jjj = jj + 1; jjj < m; jjj++) {
+								for (int jjj = 0; jjj < m; jjj++) {
 									if (map[iii][jjj] == 0) {
 										map[iii][jjj] = 1;
 										bfs();
-										ans = max(ans, counting());
 										map[iii][jjj] = 0;
 									}
 								}
